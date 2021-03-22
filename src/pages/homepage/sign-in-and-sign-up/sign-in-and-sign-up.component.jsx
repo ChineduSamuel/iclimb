4a8e0053e2f0d274ef7dccdf38aaefbd757eed19
Chinedu Samuel <samuelchinedufranklin@gmail.com>
2021-03-22 21:26:57 +0100

    Created signin component

---
+++ src/pages/homepage/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+import Signin from '../../../components/signin/signin.component';
+
+import './sign-in-and-sign-up.scss';
+
+const SigninAndSignUpPage = () => (
+
+  <div className='Sign-in-and-sign-up'>
+    <Signin />
+   </div>  
+ );
+
+export default SigninAndSignUpPage;
